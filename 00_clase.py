d7b2b2e8a30730a755967887b0e95ef49b0d6eee
Daniel Frias <friasdaniel996@gmail.com>
2025-05-08 19:41:37 -0300

    primera clase de python

---
+++ 00_clase.py
@@ -0,0 +1,52 @@
+#  Variables en python
+# nombre = "Dani"
+# numero = 1
+# numero2 = 1.5
+# booleano = True
+
+# Imprime por pantalla el valor de la variable y el tipo
+# print( nombre, type(nombre))
+# print( numero, type(numero))
+# print( numero2, type(numero2))
+# print( booleano, type(booleano))
+
+# ----------------------------------------------------------------
+# condicionales
+# si
+
+# edad = 21
+
+# if edad >= 18 and edad < 65:
+#     # codigo a ejecutar si se cumple la condición.
+#     print("Usted es mayor de edad")
+# elif edad >= 65:
+#     print("Se puede jubilar")
+
+# else:
+#     print("Usted es menor de edad")
+
+# condicionales anidados (evitar)
+# if edad >= 18:
+#     # codigo a ejecutar si se cumple la condición.
+#     print("Usted es mayor de edad")
+#     if edad > 30:
+#         print("Tiene 30")
+#         if edad > 45:
+#             print("es mayor de 45")
+
+# -----------------------------------------------------------
+# Ingreso de datos por consola
+
+# print("ingrese su nombre")
+# nombre = input()
+
+# nombre = input("ingrese su nombre: ")
+# print(type(nombre))
+
+# pasar de str a int
+# numero = int(input("ingrese un numero: "))
+# print(type(numero))
+
+# numero = input("ingrese un numero: ")
+# conversor_int = int(numero)
+# print(type(conversor_int))
