41422d7760ecbc2279fa06a882c6d1e185aedc8f
Daniel Frias <friasdaniel996@gmail.com>
2025-06-30 19:47:14 -0300

    urls - vistas

---
+++ Repositorio/django_project/config/views.py
@@ -0,0 +1,30 @@
+
+from django.http import HttpResponse, JsonResponse
+
+def saludar(request):
+    return HttpResponse("<h1>Hola mundo desde mi primera vista de django!</h1>")
+
+
+def bienvenida(request):
+    return HttpResponse("Bienvenido a mi página web")
+
+
+
+def sumar(request):
+    a = int(request.GET.get("a"))
+    b = int(request.GET.get("b"))
+
+    resultado = a + b
+    
+    return JsonResponse({
+            "resultado": resultado
+    })
+
+
+
+"""
+    {
+        "clave" : "valor"
+        "nombre" : "Pepito"
+    }
+"""
