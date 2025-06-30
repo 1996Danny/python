bb2b075febe52e5a62cab4367d4534fe8e3a80cb
Daniel Frias <friasdaniel996@gmail.com>
2025-06-30 19:03:24 -0300

    configuracion base del proyecto

---
+++ Repositorio/django_project/config/settings/local.py
@@ -0,0 +1,23 @@
+
+from .base import *
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-dp1$wj$hg0)13@!o(f+atfr=b)b3qw#i-y@5=1ts!3pw9#osij'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
